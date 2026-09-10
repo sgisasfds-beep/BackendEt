f24a791b93abb912d3a1781b13a65da804e71754
sgisasfds-beep <sgisasfds@gmail.com>
2026-09-10 10:59:03 -0500

    Change import of construirDocumentoEtiquetas to module

--- controllers/generarEtiquetas.js
+++ controllers/generarEtiquetas.js
@@ -1,7 +1,6 @@
 console.log("ARCHIVO generarEtiquetas.js CARGADO (versión local, sin Google Drive/Docs)");
 
 const { obtenerDimensionesPorEnvase } = require("./dimensionesEtiquetas");
-const { construirDocumentoEtiquetas } = require("./docxEtiquetas");
 const docxImportado = require("./docxEtiquetas"); // Importamos todo el módulo para revisar
 
 console.log("🔍 LO QUE NODE ESTÁ IMPORTANDO DE docxEtiquetas:", docxImportado);
